f030e8a174b8acf72caeefc43579a3435442505a
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-09-29 16:26:18 -0300

    config ts app component

--- tubeflix/src/app/app.component.ts
+++ tubeflix/src/app/app.component.ts
@@ -3,20 +3,20 @@ import { Component, HostListener } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tubeflix';
+  title = 'TeusFlix';
   navbg: any;
-  @HostListener('document:scroll') scrollover(){
-    console.log(document.body.scrollTop, 'scrolllength#');
-	
-	if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0){
-	  this.navbg = {
-	    'background-color':'#000000'
-	  }
-	} else {
-	  this.navbg = {}
-	}
+
+  @HostListener('document:scroll')
+  scrollover() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+      this.navbg = {
+        'background-color': '#000000'
+      };
+    } else {
+      this.navbg = {};
+    }
   }
 }
